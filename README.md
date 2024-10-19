5c61c93a1cb5fbfc420246395d954074e90e992e
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-10-19 14:19:34 +0530

    Front End Initialization

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # study-jams
+
 This is repo for Front End of GDG Gen AI Study Jams
+
---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Hello GDG</h1>
+    </>
+  );
+}
+
+export default App;
+
