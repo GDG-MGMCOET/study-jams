46646c30f56c5050186ca1673e6f23239388724a
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-10-20 16:10:23 +0530

    Footer Styling v1.5 - Add contributors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
+import { Tooltip } from "antd";
+
 export default function Footer() {
   return (
     <footer className="py-3 font-bold text-center bg-gray">
-      <p>© GDG On Campus MGM COET 2024</p>
+      <p>
+        © GDG On Campus MGM COET 2024 |{" "}
+        <Tooltip
+          title=" Jeetu Gupta, Hriday Budhiraja, Suraj Sharma, Tushar, Himashu Pandey"
+          placement="top"
+          className="cursor-pointer"
+        >
+          Contributors ( Hover me )
+        </Tooltip>
+      </p>
     </footer>
   );
 }
