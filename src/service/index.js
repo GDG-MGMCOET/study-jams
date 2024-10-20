deb03761fae9a634ca295b064b3a19a863230353
GDG-MGMCOET <gdg@coet.in>
2024-10-20 21:07:02 +0530

    Update index.js

--- src/service/index.js
+++ src/service/index.js
@@ -1,47 +1,23 @@
 export const leaderBoardService = {
   updateParticipantsProgress: async (data, email, password) => {
-    try {
-      const res = await fetch(
-        "https://study-jams-backend.vercel.app/admin/update-participants-status",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Basic ${btoa(`${email}:${password}`)}`, // Consider using Authorization header if needed
-          },
-          body: JSON.stringify({ participantsInfoArr: data }),
-        }
-      );
-
-      if (!res.ok) {
-        // Check for non-2xx status codes
-        throw new Error(`Error: ${res.status} ${res.statusText}`);
+    const res = await fetch(
+      "https://study-jams-backend.vercel.app/admin/update-participants-status",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, participantsInfoArr: data }),
       }
-
-      const responseData = await res.json();
-      return responseData;
-    } catch (error) {
-      console.error("Failed to update participants progress:", error);
-      return { success: false, message: error.message };
-    }
+    );
+    const responseData = await res.json();
+    return responseData;
   },
-
   getLeaderBoardData: async () => {
-    try {
-      const res = await fetch(
-        "https://study-jams-backend.vercel.app/participants"
-      );
-
-      if (!res.ok) {
-        // Check for non-2xx status codes
-        throw new Error(`Error: ${res.status} ${res.statusText}`);
-      }
-
-      const responseData = await res.json();
-      return responseData;
-    } catch (error) {
-      console.error("Failed to fetch leaderboard data:", error);
-      return { success: false, message: error.message };
-    }
+    const res = await fetch(
+      "https://study-jams-backend.vercel.app/participants"
+    );
+    const responseData = await res.json();
+    return responseData;
   },
 };
