d22c95bd5155742bf0fd1be4013b603095d7662d
Suraj Sharma <suraj200delhi@gmail.com>
2024-10-20 17:26:38 +0530

    Add react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
 import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import UploadAndParseCsv from "./components/uploadAndParseCSV.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/admin",
+    element: <UploadAndParseCsv/>,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>
 );
-
